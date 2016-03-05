6f3d33243137977da7a9fb6b7743e6d917e3fa28
INF112v16-ady006 <ady006@student.uib.no>
2016-03-05 14:04:55 +0100

    Testing ImageView

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -1,13 +1,16 @@
 package GUI;
 
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 
 /**
@@ -28,12 +31,18 @@ public class GUIMain extends Application{
         layout.setPadding(new Insets(10, 10, 10, 10));
 
         //Make the objects we want in the layout
-        Button bet = new Button("Place bet");
-        GridPane.setConstraints(bet,6,6);
+        /*Image card1 = new Image("/Clubs 1.png");
+        ImageView view1 = new ImageView(card1);
+        GridPane.setConstraints(view1,4,5);*/
 
         TextField betAmount = new TextField();
         betAmount.setPromptText("Amount to bet");
         GridPane.setConstraints(betAmount, 5, 6);
+
+        Button bet = new Button("Place bet");
+        GridPane.setConstraints(bet,6,6);
+
+
 
         //Add all the elements to the layout
         layout.getChildren().addAll(bet,betAmount);
