ea530089753e6ddd610830749dfc36b574c3beae
INF112v16-jkr028 <josteinkringlen@gmail.com>
2016-03-06 14:01:30 +0100

    Made temporary GUI files

--- src/GUI/TempGui/TempMain.java
+++ src/GUI/TempGui/TempMain.java
@@ -1,7 +1,35 @@
 package GUI.TempGui;
 
+import GUI.LayoutGenerators;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 /**
  * Created by Jostein on 06.03.2016.
  */
-public class TempMain {
+public class TempMain extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage window) throws Exception{
+
+        window.setTitle("Texas Hold'em");
+
+        BorderPane completeLayout = new BorderPane();
+
+
+
+        //Construct a new scene
+        completeLayout.setBottom(LayoutGeneratorsTemp.makePlayerLayout());
+        Scene scene = new Scene(completeLayout,1000,1000);
+
+        window.setScene(scene);
+        window.show();
+
+    }
 }
