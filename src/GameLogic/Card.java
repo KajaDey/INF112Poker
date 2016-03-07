f96047fe8cec36692073cfe28e0894d0c5989327
INF112v16-nug003 <nug003@student.uib.no>
2016-03-07 14:41:33 +0100

    Added Card class

---
+++ src/GameLogic/Card.java
@@ -0,0 +1,7 @@
+package GameLogic;
+
+/**
+ * Created by morten on 07.03.16.
+ */
+public class Card {
+}
---
+++ src/GameLogic/CardTest.java
@@ -0,0 +1,19 @@
+package GameLogic;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by morten on 07.03.16.
+ */
+public class CardTest {
+
+    @org.junit.Test
+    public void testOf() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCompareTo() throws Exception {
+
+    }
+}
