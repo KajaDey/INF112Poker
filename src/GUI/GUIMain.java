87f28463c68a9e5a531c6fd93f26b93abd60cdbb
INF112v16-jkr028 <josteinkringlen@gmail.com>
2016-03-07 17:24:28 +0100

    Deleted some useless code Made the cancel button in the game lobby close the window

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -22,26 +22,5 @@ public class GUIMain extends Application{
     public void start(Stage window) throws Exception{
         MainScreen.createSceneForMainScreen("kai-pus");
 
-        /*window.setTitle("Texas Hold'em");
-
-        GameLobby.createScreenForGameLobby();
-
-        /*BorderPane completeLayout = new BorderPane();
-        completeLayout.setPadding(new Insets(10, 10, 10, 10));
-
-        //Construct a new scene
-        completeLayout.setBottom(LayoutGenerators.makePlayerLayout());
-
-        completeLayout.setTop(LayoutGenerators.makeOpponentLayout("_Back", "_Back"));
-
-        completeLayout.setCenter(LayoutGenerators.makeBoardLayout());
-
-
-        //Scene scene = new Scene(completeLayout,800,800);
-        Scene scene = new Scene(ImageViewer.setBackground("cagey", completeLayout), 800, 800);
-
-        window.setScene(scene);
-        window.show();*/
-
     }
 }
