fb070f118d6899d3dcd94cdce5d4c32497d4e826
INF112v16-jkr028 <josteinkringlen@gmail.com>
2016-03-07 14:28:15 +0100

    Created a method for making a scene for the main screen

--- src/GUI/MainScreen.java
+++ src/GUI/MainScreen.java
@@ -1,14 +1,28 @@
 package GUI;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * Created by Jostein on 07.03.2016.
  */
 public class MainScreen {
 
-    public static Scene createScene(){
-        return null;
+    public static Scene createSceneForMainScreen(String imageName){
+        Stage window = new Stage();
+        window.setTitle("Welcome to The Game!");
+
+        BorderPane mainScreenLayout = new BorderPane();
+        mainScreenLayout.setPadding(new Insets(10,10,10,10));
+
+        Scene scene = new Scene(ImageViewer.setBackground(imageName, mainScreenLayout), 800, 800);
+
+        window.setScene(scene);
+        window.show();
+
+        return scene;
     }
 
     public static void method2(){
