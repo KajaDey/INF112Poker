a179c3cd9d4f25f50c8be80e4a93e3c39c54b65e
Morten Lohne <nug003@student.uib.no>
2016-03-08 11:58:09 +0100

    Wrote documentation for GameClient

--- src/main/java/gamelogic/GameClient.java
+++ src/main/java/gamelogic/GameClient.java
@@ -4,19 +4,38 @@ import java.util.List;
 import java.util.Map;
 
 /**
-
+A game client 
  */
 public interface GameClient {
 
+    /*
+    Tells the client to make a decision, and blocks until the decision is made and the client returns
+     */
     public Decision getDecision();
 
+    /*
+    Sends the names of all players, as a map indexed by the players' IDs
+     */
     public void setPlayerNames(Map<Integer, String> names);
 
+    /*
+    Sends the player's hole cards
+     */
     public void setHoleCards(Card card1, Card card2);
 
+    /*
+    Sends the stack sizes of all players, as a map indexed by the players' IDs
+     */
     public void setStackSizes(Map<Integer, Long> stackSizes);
 
-    public void playerMadeDecision(Map<Integer, Decision> decisions);
+    /*
+    Sends the decision of another player
+     */
+    public void playerMadeDecision(Integer playerId, Decision decision);
 
+    /*
+    After a showdown, the client receives the hole cards of all the players still in the hand,
+    as a map index by the players' IDs
+     */
     public void showdown(Map<Integer, List<Card>> holeCards);
 }
