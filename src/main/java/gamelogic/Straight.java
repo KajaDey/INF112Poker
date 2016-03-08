d4c284c0560e8a68740d7ac506a6bb9644e37bb5
INF112v16-raa009 <raa009@student.uib.no>
2016-03-08 16:05:01 +0100

    Implemented Straight, almost done

--- src/main/java/gamelogic/Straight.java
+++ src/main/java/gamelogic/Straight.java
@@ -1,12 +1,31 @@
 package main.java.gamelogic;
 
+import java.util.List;
+
 /**
  * Created by pokki on 08/03/16.
  */
 public class Straight implements IRule {
 
+    private int drawCount = 0;
+
     @Override
     public boolean match(Hand hand) {
-        return false;
+
+        // TODO: finn BESTE straight
+        // TODO: tenk på ess, rank 14
+
+        List<Card> cards = hand.getAllCards();
+        cards.sort(Card::compareTo);
+
+        for (int i = 0; i < cards.size() - 1; i++) {
+            if (cards.get(i).rank == cards.get(i+1).rank - 1) {
+                drawCount++;
+            }
+            else {
+                drawCount = 0;
+            }
+        }
+        return drawCount >= 5;
     }
 }
