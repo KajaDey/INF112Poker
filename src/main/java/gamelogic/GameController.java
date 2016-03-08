5f9fc9a6e3389c8e36dad69f9e64b0c8cdb06784
INF112v16-kro050 <kro050@student.uib.no>
2016-03-08 13:43:23 +0100

    Added the initial TODOs in GameController to make the intended flow visible for everyone

--- src/main/java/gamelogic/GameController.java
+++ src/main/java/gamelogic/GameController.java
@@ -8,11 +8,26 @@ import main.java.gui.GUIMain;
 public class GameController {
 
     public static void Main (String [] args) {
-        //TODO: Display settings screen
-        GUIMain gui = new GUIMain();
-
-
+        //TODO: Display the first screen (login screen)
+        // new GUI(this)
     }
 
+    public void LoginButtonClicked() { //TODO: Set the parameters here
+        //TODO: Validate input
+        //TODO: Tell GUI to set screen to Lobby screen
+    }
 
+    public void StartTournamentButtonClicked() { //TODO: Set the parameters we need here
+        //TODO: Validate the data, if invalid, report back to GUI
+        //TODO: Make AIs for remaining players (1 AI in first iteration)
+        //TODO: Make a new Game object
+
+        //GUIGameClient
+        //TODO: Tell GUI to set the screen to Table screen. ** This must return a GameClient-object! **
+        //TODO: Tell GUIGameClientObject what to display in the table screen, using the init-method from GameClient-interface
+
+        //AIGameClient
+        //TODO: Make an AIGameClient-object
+        //TODO: Tell the AIGameClient-object whats up with the table using the init-method from GC-interface
+    }
 }
