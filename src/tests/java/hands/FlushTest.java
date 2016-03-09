de34bd087aa9f11bb2cd0e2944129a0f93d61bd8
INF112v16-vro006 <vegen_94@hotmail.com>
2016-03-09 16:06:32 +0100

    Have made a test package for hands, and made a test class for Flush

---
+++ src/tests/java/hands/FlushTest.java
@@ -0,0 +1,27 @@
+package tests.java.hands;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vegar on 09/03/16.
+ */
+public class FlushTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMatch() throws Exception {
+
+    }
+
+    @Test
+    public void testGetHand() throws Exception {
+
+    }
+}
