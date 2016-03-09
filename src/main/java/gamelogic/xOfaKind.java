e0bff386224166369adf3fa478cfec60a88dd8f2
INF112v16-jkr028 <josteinkringlen@gmail.com>
2016-03-09 13:22:21 +0100

    Changed name of a class declaration

--- src/main/java/gamelogic/xOfaKind.java
+++ src/main/java/gamelogic/xOfaKind.java
@@ -8,7 +8,7 @@ import java.util.List;
  *
  * NOT FINISHED!
  */
-public class XofaKind implements IRule {
+public class xOfaKind implements IRule {
     private boolean FourOfaKind;
     private boolean ThreeOfaKind;
     private boolean TwoOfaKind;
