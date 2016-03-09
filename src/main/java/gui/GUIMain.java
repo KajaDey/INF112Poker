97219d5371ab34bbb3e88a9ad202828cf4b675b1
Jostein Kringlen <josteinkringlen@gmail.com>
2016-03-09 16:24:20 +0100

    Changes wasn't necessary anyways

--- src/main/java/gui/GUIMain.java
+++ src/main/java/gui/GUIMain.java
@@ -12,9 +12,9 @@ import java.util.Map;
  */
 public class GUIMain extends Application{
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
-    }*/
+    }
 
     public static void run(String[] args){
         launch(args);
