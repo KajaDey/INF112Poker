d3094af8ccd7b9e1705b4848c243dbf6cc1da114
INF112v16-raa009 <raa009@student.uib.no>
2016-03-10 11:52:30 +0100

    Working on StraightFlush

--- src/main/java/gamelogic/rules/StraightFlush.java
+++ src/main/java/gamelogic/rules/StraightFlush.java
@@ -13,16 +13,46 @@ import java.util.Optional;
 public class StraightFlush implements IRule {
 
     private List<Card> returnHand = new ArrayList<>();
+    private List<Card> mainSuitCards = new ArrayList<>();
     private List<Card> cards;
 
     @Override
     public boolean match(Hand hand) {
         cards = hand.getAllCards();
 
+        Card.Suit mainSuit = findMainSuit();
+        Card lastCardAdded = null;
+
+        for (Card c : cards) {
+            if (c.suit == mainSuit) {
+                mainSuitCards.add(c);
+                lastCardAdded = c;
+            }
+        }
+
+        if (mainSuitCards.size() < 5) {
+            return false;
+        }
+
+        while (mainSuitCards.size() < 7) {
+            mainSuitCards.add(lastCardAdded);
+        }
+
+        // handWithMinSuit inneholder alle i mainSuit
+//         hand = new Hand(card1, card2, Arrays.asList(card3, card4, card5, card7, card6));
+
+        // Send returnHand inn i straight
+        Straight straight = new Straight();
+//        straight.match(returnHand);
+
+        // sjekk om true
+
+
 
 
         return false;
     }
+
 
     @Override
     public Optional<List<Card>> getHand() {
@@ -30,4 +60,10 @@ public class StraightFlush implements IRule {
             return Optional.of(returnHand);
         }
         return Optional.empty();
-    }}
+    }
+
+    private Card.Suit findMainSuit() {
+        // TODO
+        return Card.Suit.CLUBS;
+    }
+}
