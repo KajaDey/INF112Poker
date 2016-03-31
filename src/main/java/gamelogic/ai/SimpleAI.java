c19218376a5250ba0e928eeada4ab96bf113ce47
Morten Lohne <nug003@student.uib.no>
2016-03-31 17:55:10 +0200

    AI will now assert that it is not sent a negative stack

--- src/main/java/gamelogic/ai/SimpleAI.java
+++ src/main/java/gamelogic/ai/SimpleAI.java
@@ -206,7 +206,10 @@ public class SimpleAI implements GameClient {
     @Override
     public void setStackSizes(Map<Integer, Long> stackSizes) {
         assert stackSizes.size() == amountOfPlayers;
-
+        assert stackSizes.get(this.playerId) >= 0 : "AI was sent a stacksize of " + stackSizes.get(this.playerId);
+        for (int playerId : stackSizes.keySet()) {
+            assert stackSizes.get(playerId) >= 0 : "Player " + playerId + "'s stacksize is " + stackSizes.get(playerId);
+        }
         this.stackSize = stackSizes.get(this.playerId);
         this.stackSizes = stackSizes;
     }
