9c64f5b4060114a5b314dd332b9c9025833329c9
Ragnhild Aalvik <raa009@student.uib.no>
2016-05-05 19:21:31 +0200

    Removed delay when playing with SimpleAI

--- src/test/java/gamelogic/GameTest.java
+++ src/test/java/gamelogic/GameTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import gui.GUIMain;
 import gui.GameSettings;
-import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -13,10 +12,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.ArrayList;
 
 import static org.mockito.Matchers.*;
-import static org.powermock.api.mockito.PowerMockito.doNothing;
-import static org.powermock.api.mockito.PowerMockito.spy;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
-import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 
 /**
@@ -26,38 +22,16 @@ import static org.powermock.api.support.membermodification.MemberMatcher.method;
 @PrepareForTest({Game.class, GameController.class, GUIMain.class})
 
 public class GameTest {
-    private GameController gameController, gameControllerSpy;
-    private Game game, gameSpy;
-
-    /**
-     * Creates a mock game controller for use when testing hard coded decision
-     */
-    private void createGameControllerMock() {
-        gameController = PowerMockito.mock(GameController.class);
-        PowerMockito.doReturn(new Decision(Decision.Move.ALL_IN)).when(gameController).getDecisionFromClient(anyInt());
-    }
-
-    /**
-     * Creates a game controller spy for use when there are AI players
-     */
-    private void createGameControllerSpy() {
-        gameController = spy(new GameController());
-    }
-
-    /**
-     * Creates a game spy so we can run playGame()
-     */
-    private void createGameSpy() throws Exception {
-        game = new Game(new GameSettings(5000, 50, 25, 2, 10, AIType.MCTS_AI), gameController);
-        gameSpy = spy(game);
-
-        doNothing().when(gameSpy, "delay", anyLong());
-    }
+    private GameController gameControllerMock, gameControllerSpy;
+    private Game gameSpy;
 
     @Test
-    public void testMockTwoPlayersBothAllIn() throws Exception {
-        createGameControllerMock();
-        createGameSpy();
+    public void testGameSpyTwoPlayersBothAllIn() throws Exception {
+        gameControllerMock = mock(GameController.class);
+        PowerMockito.doReturn(new Decision(Decision.Move.ALL_IN)).when(gameControllerMock).getDecisionFromClient(anyInt());
+
+        gameSpy = spy(new Game(new GameSettings(5000, 50, 25, 2, 10, AIType.MCTS_AI), gameControllerMock));
+        doNothing().when(gameSpy, "delay", anyLong());
 
         gameSpy.addPlayer("Ragnhild", 0);
         gameSpy.addPlayer("Kristian", 1);
@@ -66,21 +40,32 @@ public class GameTest {
     }
     @Test
     public void testPlayGameWithSixSimpleAIs() throws Exception {
-        GameSettings gameSettings = new GameSettings(5000, 50, 25, 6, 10, AIType.SIMPLE_AI);
-        gameController = new GameController(gameSettings);
-        gameSpy = spy(new Game(gameSettings, gameController));
-        doNothing().when(gameSpy, "delay", anyLong());
+        GameSettings gameSettings = new GameSettings(5000, 500, 250, 6, 10, AIType.SIMPLE_AI);
+        gameControllerSpy = spy(new GameController(gameSettings));
+        doNothing().when(gameControllerSpy).delay(anyLong());
 
+        gameSpy = spy(new Game(gameSettings, gameControllerSpy));
+        doNothing().when(gameSpy, "delay", anyLong());
         whenNew(Game.class).withArguments(any(GameSettings.class), any(GameController.class)).thenReturn(gameSpy);
 
-        Thread gameThread = gameController.initGame(false, new ArrayList<>());
+        Thread gameThread = gameControllerSpy.initGame(false, new ArrayList<>());
         gameThread.join();
     }
 
     @Test
     public void testPlayGameWithSixMCTSAIs() throws Exception {
         gameControllerSpy = spy(new GameController());
-//        doNothing().when(gameControllerSpy, "delay", anyLong());
+
+//        doAnswer(new Answer<Decision>() {
+//            @Override
+//            public Decision answer(InvocationOnMock aiClient) throws Throwable{
+//                return null;//(GameClient)aiClient.getDecision(timeToTake);
+//            }
+//        }).when(gameControllerSpy).getAIDecision(anyObject());
+//
+
+
+        // override getAIDecision
         doNothing().when(gameControllerSpy).delay(anyLong());
 
         gameSpy = spy(new Game(new GameSettings(GameSettings.DEFAULT_SETTINGS), gameControllerSpy));
