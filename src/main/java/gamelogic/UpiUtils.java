318863c132f6234b0a0960f1a0ef78384c3b835d
Morten Lohne <nug003@student.uib.no>
2016-05-07 17:27:31 +0200

    Added a new class UpiUtils for utility methods related to sending upi commands

---
+++ src/main/java/gamelogic/UpiUtils.java
@@ -0,0 +1,7 @@
+package gamelogic;
+
+/**
+ * Created by morten on 07.05.16.
+ */
+public class UpiUtils {
+}
---
+++ src/test/java/gamelogic/UpiUtilsTest.java
@@ -0,0 +1,16 @@
+package gamelogic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by morten on 07.05.16.
+ */
+public class UpiUtilsTest {
+
+    @Test
+    public void testTokenize() throws Exception {
+
+    }
+}
