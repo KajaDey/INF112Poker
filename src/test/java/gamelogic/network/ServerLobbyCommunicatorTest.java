b823f858458060e0c79cdba34b3c0db159744a6a
Ragnhild Aalvik <raa009@student.uib.no>
2016-05-10 21:28:31 +0200

    Finished most important tests for network

---
+++ src/test/java/gamelogic/network/ServerLobbyCommunicatorTest.java
@@ -0,0 +1,72 @@
+package gamelogic.network;
+
+import gamelogic.Logger;
+import gui.LobbyScreen;
+import javafx.application.Platform;
+import network.Server;
+import network.ServerLobbyCommunicator;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.Stack;
+
+import static org.mockito.Matchers.any;
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.powermock.api.mockito.PowerMockito.doNothing;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+/**
+ * Created by pokki on 10/05/16.
+ */
+public class ServerLobbyCommunicatorTest {
+    private ServerLobbyCommunicator lobbyCommunicator;
+    public static Stack<String> inputStrings;
+
+    @Test
+    public void testServerLobbyCommunicator() throws Exception {
+        setupServerLobbyTest();
+
+        lobbyCommunicator.start();
+
+        Thread commThread = lobbyCommunicator.getListeningThread();
+        commThread.join();
+    }
+
+    private void setupServerLobbyTest() throws Exception {
+        new Server();
+
+        inputStrings = new Stack<>();
+        inputStrings.addAll(Arrays.asList("startGame", "playerJoinedTable 0 0",
+                "tableSettings 0 smallBlind 25 bigBlind 100 maxNumberOfPlayers 2",
+                "tableCreated 0", "playerJoinedLobby 0 Ragnhild", "lobbySent", "lobbyok"));
+
+        createServerLobbyCommunicatorSpy();
+    }
+
+    private void createServerLobbyCommunicatorSpy() throws Exception {
+        try {
+            lobbyCommunicator = spy(new ServerLobbyCommunicator("Ragnhild", mock(LobbyScreen.class), InetAddress.getLocalHost(), new Logger()));
+
+            doNothing().when(lobbyCommunicator, "goToGameScreen");
+
+            doAnswer(new Answer<String>() {
+                @Override
+                public String answer(InvocationOnMock invocation) throws Throwable {
+                    if (inputStrings.isEmpty()) System.exit(0);
+
+                    return inputStrings.pop();
+                }
+            }).when(lobbyCommunicator, "readFromServer");
+
+            mockStatic(Platform.class);
+            doNothing().when(Platform.class, "runLater", any(Runnable.class));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
